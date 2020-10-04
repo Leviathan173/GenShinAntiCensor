3f22a310581517f42bf3616b73ced7c7894e44ae
Leviathan173 <hft0149@Gmail.com>
2020-10-05 01:36:50 +0800

    上传脚本

---
+++ ys.bat
@@ -0,0 +1,11 @@
+REM
+chcp 65001
+echo= > "D:\Game\原神\Genshin Impact\Genshin Impact Game\YuanShen_Data\Persistent\AssetBundles\blocks\00\29342328.blk"
+
+powershell.exe -command "start \"D:\Game\原神\Genshin Impact\Genshin Impact Game\YuanShen.exe\""
+
+timeout /t 180
+
+echo= > "D:\Game\原神\Genshin Impact\Genshin Impact Game\YuanShen_Data\Persistent\AssetBundles\blocks\00\29342328.blk"
+
+pause
